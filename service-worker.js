3583939fdd83734ea3ea822f550bcc9a4045feb7
Rafael Ramos <contactorafael05@gmail.com>
2025-09-19 18:20:07 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,32 @@
-const CACHE_NAME = 'chitre-selecto-cache-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png',
-  'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
-];
+const CACHE_NAME = 'app-cache-v' + new Date().getTime();
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Cache abierto');
-        return cache.addAll(urlsToCache);
-      })
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(['index.html','manifest.json']);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.map(k => { if (k !== CACHE_NAME) return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
       .then(response => {
-        if (response) {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(e.request, response.clone());
           return response;
-        }
-        return fetch(event.request);
+        });
       })
+      .catch(() => caches.match(e.request))
   );
 });
